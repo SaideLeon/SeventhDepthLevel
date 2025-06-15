5baddcfe359f141613412a416695b534503c2dd7
Saíde Omar Saíde <saideomarsaideleon@gmail.com>
2025-06-15 22:09:50 +0000

    Não foi aplicada bem a altura do sidebar

--- src/components/chat-interface.tsx
+++ src/components/chat-interface.tsx
@@ -547,7 +547,7 @@ export default function ChatInterface() {
             <span className="group-data-[collapsible=icon]:hidden">Nova Conversa</span>
           </Button>
         </SidebarHeader>
-        <ScrollArea className="flex-1">
+        <ScrollArea className="flex-1 min-h-0">
           <div className="p-1 pt-0">
             <SidebarMenu>
               {sortedSessions.map((session) => (
