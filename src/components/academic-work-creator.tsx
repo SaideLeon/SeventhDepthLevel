156689b5840f270f9799b15c94a82b801f4864ab
Saíde Omar Saíde <saideomarsaideleon@gmail.com>
2025-06-21 05:38:26 +0000

    no criador: nenhum elemento filho deve quebrar ou forçar a altura o comp

--- src/components/academic-work-creator.tsx
+++ src/components/academic-work-creator.tsx
@@ -617,7 +617,7 @@ export default function AcademicWorkCreator({ activeWork, onUpdateWork }: Academ
 
 
   return (
-    <div className="flex flex-row h-full bg-background text-foreground">
+    <div className="flex flex-row flex-1 min-h-0 bg-background text-foreground">
       <div className="flex flex-col flex-1 min-w-0">
         <ScrollArea className="flex-1" viewportRef={workAreaRef}>
          <div className="p-3 md:p-4 space-y-4">
